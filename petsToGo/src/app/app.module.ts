609d854a64925374228a8bc32f7054a6504d202d
GabyGV <56004250+GabyGV@users.noreply.github.com>
2022-09-27 09:34:15 -0600

    New angular project
    Creation of first page

--- petsToGo/src/app/app.module.ts
+++ petsToGo/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first.component';
+import { AlertModule } from '@coreui/angular';
+import { HeaderModule, GridModule, NavModule, DropdownModule, CardModule, ButtonModule, ImgModule } from '@coreui/angular';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule,
+    HeaderModule,
+    GridModule, 
+    NavModule, 
+    DropdownModule,
+    CardModule,
+    ButtonModule,
+    ImgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
